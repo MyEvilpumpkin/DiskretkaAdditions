55204fd7afb2c3ea1835cbebaf21e119c3dcfb4f
MyEvilpumpkin <dima.tomin2010@yandex.ru>
2019-04-20 03:21:26 +0300

    Исправлена функция renew

--- DiskretkaAdditions/TemplateOperations.h
+++ DiskretkaAdditions/TemplateOperations.h
@@ -3,15 +3,21 @@
 
 template<class T>
 T* renew(T*& obj, const size_t oldSize, const size_t newSize) {
-	T* newObj;
-	if (newSize > 0) {
-		newObj = new T[newSize];
-		if (oldSize > 0)
-			memcpy(newObj, obj, oldSize * sizeof(T));
+	if (oldSize != newSize) {
+		T* newObj;
+		if (newSize > 0) {
+			newObj = new T[newSize];
+			if (oldSize > 0)
+				if (oldSize < newSize)
+					memcpy(newObj, obj, oldSize * sizeof(T));
+				else
+					memcpy(newObj, obj, newSize * sizeof(T));
+		}
+		else
+			newObj = nullptr;
+		delete[] obj;
+		obj = newObj;
 	}
-	else
-		newObj = nullptr;
-	delete[] obj;
-	obj = newObj;
 	return obj;
 }
+
